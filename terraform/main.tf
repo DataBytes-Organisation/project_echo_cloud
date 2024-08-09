b727c2864fc2418c1350dcc202612a882694c972
Neel050302 <104186890+Neel050302@users.noreply.github.com>
2024-08-09 20:01:11 +1000

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,9 @@
+provider "google" { 
+
+  project = "sit-23t1-project-echo-25288b9" # replace with your project name if different 
+
+  region  = "australia-southeast2" 
+
+  zone    = "australia-southeast2-a" 
+
+} 
