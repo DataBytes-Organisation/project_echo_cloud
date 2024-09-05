044a45791fc4314106f6aa8bb1251b8f05d75698
Adam <amedwards@deakin.edu.au>
2024-09-05 21:58:10 +1000

    testing external facing service with https and load balancer

--- terraform/root_modules/dns_and_ssl/dns.tf
+++ terraform/root_modules/dns_and_ssl/dns.tf
@@ -6,3 +6,18 @@ resource "google_dns_record_set" "dns_authorization_wildcard_certificate" {
   ttl          = 300
   rrdatas      = [google_certificate_manager_dns_authorization.dns_authorization.dns_resource_record[0].data]
 }
+
+# ip address for external_services load balancer which is created in kubernetes/manifests/ingress/fanout-ingress.yaml
+resource "google_compute_global_address" "external_services" {
+  name = "external-services"
+}
+
+resource "google_dns_record_set" "services" {
+  name = "services.${var.public_domain_name}."
+  type = "A"
+  ttl  = 21600
+
+  managed_zone = var.managed_zone_name
+
+  rrdatas = [google_compute_global_address.external_services.address]
+}
