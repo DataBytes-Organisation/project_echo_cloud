61c6a7062e9498d25192279878a208e6ccc9d241
Liam <echoEclectus@users.noreply.github.com>
2025-04-05 13:23:42 +1100

    Add lifecycle rule and state encryption

--- terraform/root_modules/terraform_state_bucket/bucket.tf
+++ terraform/root_modules/terraform_state_bucket/bucket.tf
@@ -7,4 +7,32 @@ resource "google_storage_bucket" "tooling_state" {
   versioning {
     enabled = true
   }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 90 # Auto-delete old state files after 90 days
+    }
+  }
+
+  encryption {
+    default_kms_key_name = google_kms_crypto_key.terraform_state_key.id
+  }
+}
+
+# KMS key for state encryption
+resource "google_kms_crypto_key" "terraform_state_key" {
+  name     = "terraform-state-key"
+  key_ring = google_kms_key_ring.terraform_key_ring.id
+  purpose  = "ENCRYPT_DECRYPT"
+  lifecycle {
+    prevent_destroy = true # Makes KMS key undeletable
+  }
+}
+
+resource "google_kms_key_ring" "terraform_key_ring" {
+  name     = "terraform-state-keyring"
+  location = var.gcp_default_region
 }
